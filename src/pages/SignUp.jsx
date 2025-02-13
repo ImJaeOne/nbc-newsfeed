284bf899406ecfb6ef75dd398ac2e62237e0bf65
lje00220 <155710708+lje00220@users.noreply.github.com>
2025-02-13 18:39:57 +0900

    feat: 비밀번호 확인 로직 추가, 비밀번호 input type 변경

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,12 +6,17 @@ const SignUp = () => {
   const [userNickname, setUserNickname] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const navigate = useNavigate();
 
   // form 제출 시 Supabase를 통해 회원가입을 하는 함수
   const handleSignup = async (e) => {
     e.preventDefault();
+    if (password !== confirmPassword) {
+      alert('비밀번호를 다시 확인해주세요!');
+      return;
+    }
     try {
       await supabase.auth.signUp({
         email,
@@ -39,7 +44,7 @@ const SignUp = () => {
         <br />
         이메일:{' '}
         <input
-          type="text"
+          type="email"
           name="email"
           id="email"
           value={email}
@@ -48,11 +53,20 @@ const SignUp = () => {
         <br />
         비밀번호:{' '}
         <input
-          type="text"
+          type="password"
           name="pwd"
           id="pwd"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+        />{' '}
+        <br />
+        비밀번호 확인:{' '}
+        <input
+          type="password"
+          name="confirmPwd"
+          id="confirmPwd"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
         />{' '}
         <br />
         <button>확인</button>
