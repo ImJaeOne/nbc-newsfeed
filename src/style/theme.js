eda84800fc50774af147bfece2447e0f97357d2d
woohyuckk <woohyuckk@gmail.com>
2025-02-13 11:53:37 +0900

    style: 공통색상팔레트 추가

---
+++ src/style/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    nav_background_color: '#FEE3A2',
+    body_background_color: '#FFFBF0',
+    modal_background_color: '',
+    button_color: '',
+    like_color: '#ec4c63',
+  },
+};
