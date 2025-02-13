f38d2eb155e1f925dc11a4190ce243f437742dbc
woohyuckk <woohyuckk@gmail.com>
2025-02-13 21:49:27 +0900

    chore: assets에 logo 추가

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,10 +5,7 @@ const Header = () => {
   return (
     <S.HeaderContainer>
       <S.Link to="/">
-        <S.Logo
-          src="https://private-user-images.githubusercontent.com/192562150/412856181-af9e8b8c-3202-4b86-b442-08a3274e9af8.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mzk0NDQ5MDcsIm5iZiI6MTczOTQ0NDYwNywicGF0aCI6Ii8xOTI1NjIxNTAvNDEyODU2MTgxLWFmOWU4YjhjLTMyMDItNGI4Ni1iNDQyLTA4YTMyNzRlOWFmOC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwMjEzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDIxM1QxMTAzMjdaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT00Y2ZhMjBhZmEwYzRmYzVlYjg3ZGFmNzA2NTI3YmNmNDljNGYzMmUwODU4NTliYmVkZjIyOTc3ZmYwZDI2YmY4JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.Dgn51EJ7UP7YmEKZqIvx5Oq-HDZYrjg8zEMv4ASNTP4"
-          alt="Logo"
-        />
+        <S.Logo src="/src/assets/logo.png " alt="Logo" />
       </S.Link>
       <Nav />{' '}
     </S.HeaderContainer>
