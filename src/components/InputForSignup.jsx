70319620c17d1246ecacdbbd112f38e81170012d
PureunKang <pureun0000@gmail.com>
2025-02-14 16:19:11 +0900

    docs : 파일명 변경에 따른 파일 삭제

--- src/components/InputForSignup.jsx
+++
@@ -1,7 +0,0 @@
-import S from '../style/SignUp/SignUp.Input.style';
-
-const InputForSignup = ({ type, value, onChange }) => {
-  return <S.SignUpInput type={type} value={value} onChange={onChange} />;
-};
-
-export default InputForSignup;
