42bb7261a817de5106dce6dcb7bb6e376d0d1a10
woohyuckk <woohyuckk@gmail.com>
2025-02-14 14:23:35 +0900

    style: logoutbtn style 추가

--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -6,19 +6,16 @@ import { supabase } from '../../supabase/client';
 const Nav = () => {
   const { isLogin, user } = useContext(AuthContext);
 
-  console.log(user);
+  function logoutInNavBar() {
+    supabase.auth.signOut();
+  }
+
   return (
     <S.Nav>
       {isLogin ? (
         <S.UserMenu>
           <S.UserName to="/mypage">{user.nickname}</S.UserName>
-          <S.Logoutbtn
-            onClick={() => {
-              supabase.auth.signOut();
-            }}
-          >
-            로그아웃
-          </S.Logoutbtn>
+          <S.Logoutbtn onClick={logoutInNavBar}>로그아웃</S.Logoutbtn>
         </S.UserMenu>
       ) : (
         <S.AuthLinks>
