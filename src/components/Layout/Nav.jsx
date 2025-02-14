5747e8beb7fd8a0addcfa44abd461e26d8867db3
woohyuckk <woohyuckk@gmail.com>
2025-02-14 14:25:06 +0900

    chore: 로그아웃 함수명 변경 logoutInNav->handleLogout

--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -6,7 +6,7 @@ import { supabase } from '../../supabase/client';
 const Nav = () => {
   const { isLogin, user } = useContext(AuthContext);
 
-  function logoutInNavBar() {
+  function handleLogout() {
     supabase.auth.signOut();
   }
 
@@ -15,7 +15,7 @@ const Nav = () => {
       {isLogin ? (
         <S.UserMenu>
           <S.UserName to="/mypage">{user.nickname}</S.UserName>
-          <S.Logoutbtn onClick={logoutInNavBar}>로그아웃</S.Logoutbtn>
+          <S.Logoutbtn onClick={handleLogout}>로그아웃</S.Logoutbtn>
         </S.UserMenu>
       ) : (
         <S.AuthLinks>
