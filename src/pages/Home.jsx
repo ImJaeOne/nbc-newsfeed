b3a8976a6b3f7bd4140b539a1c1354d0c6cb3a9d
ImJaeOne <dlawi0108@naver.com>
2025-02-15 14:09:06 +0900

    refactor: posts 데이터 불러오는 부분 주석으로 설명

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,13 +11,30 @@ const Home = () => {
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
+    /**
+     * 게시글 데이터를 불러오는 함수
+     * @async
+     * @function fetchPostData
+     * @returns {Promise<void>} - Supabase에서 데이터를 가져와 상태를 업데이트함
+     */
     const fetchPostData = async () => {
+      /**
+       * Supabase에서 posts 테이블의 데이터를 가져옴
+       * - users 테이블에서 user_num과 일치하는 user_nickname을 포함
+       * - post_like 테이블에서 post_num과 일치하는 좋아요 정보 포함
+       * @type {{ data: Object[], error: Error }}
+       */
       try {
         const { data, error } = await supabase.from('posts').select(`
     *, 
-    users:user_num(user_nickname), 
+    users: user_num(user_nickname), 
     post_like(post_num)
   `);
+        /**
+         * post_date를 변환한 후 최신 순으로 정렬
+         * 현재 시간 기준으로 시간 계산
+         * @type {Object[]}
+         */
         const sortedPosts = data
           .map((post) => ({
             ...post,
@@ -34,8 +51,6 @@ const Home = () => {
     };
     fetchPostData();
   }, []);
-
-  console.log(posts);
 
   return (
     <HomeContainer>
@@ -93,8 +108,17 @@ const Card = styled.div`
   flex-direction: column;
   gap: 10px;
   background-color: #fee3a2;
-  border: 1px solid #ccc;
+  border: 1px solid #d2a679;
   border-radius: 5px;
+  transition:
+    border 0.3s,
+    box-shadow 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    border: 1px solid #b88650;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
+  }
 `;
 
 const CardHeader = styled.div`
