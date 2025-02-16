ab49866454b4b039bce2c62efa4f631e95833cd3
ImJaeOne <dlawi0108@naver.com>
2025-02-17 00:39:20 +0900

    chore: 이미지

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,14 @@
 import Nav from './Nav';
+import logo from '/logo.png';
 import { S } from '../../style/StLayout/Header.style';
+
+const LOGO_IMAGE = logo;
 
 const Header = () => {
   return (
     <S.HeaderContainer>
       <S.Link to="/">
-        <S.Logo src="/logo.png" alt="Logo" />
+        <S.Logo src={LOGO_IMAGE} alt="Logo" />
       </S.Link>
       <Nav />
     </S.HeaderContainer>
