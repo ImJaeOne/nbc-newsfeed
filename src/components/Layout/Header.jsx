ba21ec8ded8e6c9db40d9ec141670b5bbd416fb8
ImJaeOne <dlawi0108@naver.com>
2025-02-17 00:35:03 +0900

    chore: 이미지 경로 src에 직접

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,11 @@
 import Nav from './Nav';
-import logo from '/logo.png';
 import { S } from '../../style/StLayout/Header.style';
 
 const Header = () => {
   return (
     <S.HeaderContainer>
       <S.Link to="/">
-        <S.Logo src={logo} alt="Logo" />
+        <S.Logo src="/logo.png" alt="Logo" />
       </S.Link>
       <Nav />
     </S.HeaderContainer>
