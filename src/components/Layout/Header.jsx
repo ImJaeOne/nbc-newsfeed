27b866c7ab60a931c537d97f32da4eeeef585b47
ImJaeOne <dlawi0108@naver.com>
2025-02-17 00:30:03 +0900

    chore: import 소문자로 변경

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,12 @@
 import Nav from './Nav';
-import Logo from '/logo.png';
+import logo from '/logo.png';
 import { S } from '../../style/StLayout/Header.style';
 
 const Header = () => {
   return (
     <S.HeaderContainer>
       <S.Link to="/">
-        <S.Logo src={Logo} alt="Logo" />
+        <S.Logo src={logo} alt="Logo" />
       </S.Link>
       <Nav />
     </S.HeaderContainer>
