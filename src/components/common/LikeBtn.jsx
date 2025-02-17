9f04e898a5fa25a1dad25f3251b2038e2cface00
ImJaeOne <dlawi0108@naver.com>
2025-02-17 16:24:55 +0900

    chore: 불필요한 import 삭제

--- src/components/common/LikeBtn.jsx
+++ src/components/common/LikeBtn.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IconBtn } from './IconBtn';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import styled from 'styled-components';
