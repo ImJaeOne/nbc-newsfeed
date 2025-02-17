63774ef0d75588a6115341629562008c22bffed1
woohyuckk <woohyuckk@gmail.com>
2025-02-17 20:02:26 +0900

    refact: Route 체크완료

--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -10,15 +10,15 @@ const Nav = () => {
   function handleLogout() {
     supabase.auth.signOut();
   }
-
   return (
     <S.NavContainer>
-      {isLogin === LOGIN_STATUS.LOGGED_IN ? (
+      {isLogin === LOGIN_STATUS.LOGGED_IN && (
         <S.UserMenu>
           <S.UserName to="/mypage">{user.nickname}</S.UserName>
           <S.Logoutbtn onClick={handleLogout}>로그아웃</S.Logoutbtn>
         </S.UserMenu>
-      ) : (
+      )}{' '}
+      {isLogin === LOGIN_STATUS.LOGGED_OUT && (
         <S.LinksWrapper>
           <S.AuthLink to="/login">로그인</S.AuthLink>
           <S.AuthLink to="/signup">회원가입</S.AuthLink>
--- src/shared/ProtectedRoute.jsx
+++ src/shared/ProtectedRoute.jsx
@@ -9,22 +9,18 @@ const ProtectedRoute = () => {
   const { pathname } = useLocation();
 
   if (isLogin !== LOGIN_STATUS.UNAUTHORIZED) {
-    switch (isLogin) {
-      case LOGIN_STATUS.UNAUTHORIZED:
-      case LOGIN_STATUS.LOGGED_OUT: // 로그아웃 상태도 로그인 페이지로 이동
-        return (
-          <Navigate to="/login" replace state={{ redirectedFrom: pathname }} />
-        );
-
-      case LOGIN_STATUS.LOGGED_IN:
-        return <Outlet />;
-
-      default:
-        // 예기치 못한 상태일 경우 안전한 기본 처리
-        return <Navigate to="/login" replace />;
+    if (isLogin === LOGIN_STATUS.LOGGED_IN) {
+      return <Outlet />;
     }
+
+    if (isLogin === LOGIN_STATUS.LOGGED_OUT) {
+      return (
+        <Navigate to="/login" replace state={{ redirectedFrom: pathname }} />
+      );
+    }
+  } else {
+    return <Loading />;
   }
-  return <Loading />;
 };
 
 export default ProtectedRoute;
