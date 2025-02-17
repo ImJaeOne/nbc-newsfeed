3decc5c01cb0bcc3beba1a1270b87581a6b4de84
PureunKang <pureun0000@gmail.com>
2025-02-17 15:19:51 +0900

    style : 카테고리 뱃지 공통 컴포넌트화

---
+++ src/components/common/CategoryBadge.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const S = {};
+
+export const CategoryBadge = ({ category, ...props }) => {
+  const backgroundColor = getCategoryColor(category);
+  return (
+    <S.CategoryBadge backgroundColor={backgroundColor} {...props}>
+      {category}
+    </S.CategoryBadge>
+  );
+};
+
+S.CategoryBadge = styled.span`
+  background-color: ${(props) => props.backgroundColor};
+  color: white;
+  padding: 4px 8px;
+  border-radius: 5px;
+  margin-right: 5px;
+  box-sizing: border-box;
+`;
+
+const getCategoryColor = (category) => {
+  switch (category) {
+    case '일상':
+      return '#efb2d6';
+    case '운동':
+      return '#ea9d00';
+    case '취미':
+      return '#fdecd4';
+    case '맛집':
+      return '#adcacb';
+    case '기타':
+      return '#87c159';
+    default:
+      return '#a86d83';
+  }
+};
